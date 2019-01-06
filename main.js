43022042dd51a2dbc038ef4f0d766fd6860965ab
dalalsa <sarebdesign@gmail.com>
2019-01-07 02:07:58 +0300

    creating my portfolio

--- main.js
+++ main.js
@@ -1,41 +1,135 @@
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $("html, body").animate(
-          {
-            scrollTop: target.offset().top - 80
-          },
-          1000,
-          function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) {
-              // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            }
-          }
-        );
-      }
-    }
+$(document).ready(function() {
+  var $cardGreen = $(".item.green");
+  var $cardPink = $(".item.pink");
+  var $cardBlue = $(".item.blue");
+  var $cardOrange = $("div.item.orange");
+  var infoDiv = document.querySelector(".infoDiv");
+
+  $cardGreen.hover(function() {
+    $cardGreen.toggleClass("animated flash");
   });
+  $cardPink.hover(function() {
+    $cardPink.toggleClass("animated flash");
+  });
+  $cardBlue.hover(function() {
+    $cardBlue.toggleClass("animated flash");
+  });
+  // $cardOrange.hover(function() {
+  //   $cardOrange.toggleClass("animated flash");
+  // });
+  $cardGreen.click(function() {
+    infoDiv.innerText = "hii1";
+  });
+  $cardPink.click(function() {
+    infoDiv.innerText = "hii2";
+  });
+  $cardBlue.click(function() {
+    infoDiv.innerText = "hii3";
+  });
+  $cardBlue.click(function() {
+    var CSStransforms = anime({
+      targets: "#move .blue",
+      translateX: 400  ,
+      translateY: -200,
+
+      scale: 2,
+      rotate: "2turn"
+    });
+    CSStransforms;
+  });
+  $cardOrange.click(function() {
+   
+    
+    var orangContent = "<p>Test</p>";
+   // $cardOrange.append(orangContent);
+    
+     
+    var CSStransforms = anime({
+      targets: "#move .orange",
+      translateX: 80  ,
+      translateY: -200,
+
+      scale: 1.5
+
+      // rotate: "1turn"
+    });
+    
+      $cardOrange.next(".hidden-content").slideToggle();
+    CSStransforms;
+      return false;
+
+  });
+  
+  //$hoverCard.addEventListener("mousemove", onMouseMove);
+  function onMouseMove(event) {
+    console.log("hi");
+    var infoDiv = document.querySelector(".infoDiv");
+    infoDiv.innerText = "hii";
+  }
+});
+const htmlOrange = document.querySelector("#myTemplatePetApp"); 
+
+tippy(".tooltipOrange", {
+  content: htmlOrange,
+  delay: 100,
+  arrow: true,
+  arrowType: "round",
+  size: "large",
+  duration: 500,
+  animation: "scale",
+  allowHTML: true,
+  flip: true,
+  // followCursor: true,
+  interactive: true,
+  theme: "google"
+});
+
+const htmlBlue = document.querySelector("#myTemplateEcommerceApp"); 
+
+tippy(".tooltipBlue", {
+  content: htmlBlue,
+  delay: 100,
+  arrow: true,
+  arrowType: "round",
+  size: "large",
+  duration: 500,
+  animation: "scale",
+  allowHTML: true,
+  flip: true,
+  // followCursor: true,
+  interactive: true,
+  theme: "google"
+});
+const htmlPink = document.querySelector("#myTemplateTicTacToe"); 
+
+tippy(".tooltipPink", {
+  content: htmlPink,
+  delay: 100,
+  arrow: true,
+  arrowType: "round",
+  size: "large",
+  duration: 500,
+  animation: "scale",
+  allowHTML: true,
+  flip: true,
+  // followCursor: true,
+  interactive: true,
+  theme: "google"
+});
+const htmlGreen = document.querySelector("#myTemplateMultiPlayerGame"); 
+
+tippy(".tooltipGreen", {
+  content: htmlGreen,
+  delay: 100,
+  arrow: true,
+  arrowType: "round",
+  size: "large",
+  duration: 500,
+  animation: "scale",
+  allowHTML: true,
+  flip: true,
+  // followCursor: true,
+  interactive: true,
+  theme: "google"
+});
+
