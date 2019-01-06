560782d058399c2c8373f7b69ad00266cec1eb9c
dalalsa <sarebdesign@gmail.com>
2019-01-07 02:31:48 +0300

    updatin my portfolio

--- main.js
+++ main.js
@@ -2,70 +2,21 @@ $(document).ready(function() {
   var $cardGreen = $(".item.green");
   var $cardPink = $(".item.pink");
   var $cardBlue = $(".item.blue");
-  var $cardOrange = $("div.item.orange");
-  var infoDiv = document.querySelector(".infoDiv");
+ 
 
-  $cardGreen.hover(function() {
-    $cardGreen.toggleClass("animated flash");
-  });
-  $cardPink.hover(function() {
-    $cardPink.toggleClass("animated flash");
-  });
-  $cardBlue.hover(function() {
-    $cardBlue.toggleClass("animated flash");
-  });
+  // $cardGreen.hover(function() {
+  //   $cardGreen.toggleClass("animated flash");
+  // });
+  // $cardPink.hover(function() {
+  //   $cardPink.toggleClass("animated flash");
+  // });
+  // $cardBlue.hover(function() {
+  //   $cardBlue.toggleClass("animated flash");
+  // });
   // $cardOrange.hover(function() {
   //   $cardOrange.toggleClass("animated flash");
   // });
-  $cardGreen.click(function() {
-    infoDiv.innerText = "hii1";
-  });
-  $cardPink.click(function() {
-    infoDiv.innerText = "hii2";
-  });
-  $cardBlue.click(function() {
-    infoDiv.innerText = "hii3";
-  });
-  $cardBlue.click(function() {
-    var CSStransforms = anime({
-      targets: "#move .blue",
-      translateX: 400  ,
-      translateY: -200,
-
-      scale: 2,
-      rotate: "2turn"
-    });
-    CSStransforms;
-  });
-  $cardOrange.click(function() {
-   
-    
-    var orangContent = "<p>Test</p>";
-   // $cardOrange.append(orangContent);
-    
-     
-    var CSStransforms = anime({
-      targets: "#move .orange",
-      translateX: 80  ,
-      translateY: -200,
-
-      scale: 1.5
-
-      // rotate: "1turn"
-    });
-    
-      $cardOrange.next(".hidden-content").slideToggle();
-    CSStransforms;
-      return false;
-
-  });
-  
-  //$hoverCard.addEventListener("mousemove", onMouseMove);
-  function onMouseMove(event) {
-    console.log("hi");
-    var infoDiv = document.querySelector(".infoDiv");
-    infoDiv.innerText = "hii";
-  }
+ 
 });
 const htmlOrange = document.querySelector("#myTemplatePetApp"); 
 
